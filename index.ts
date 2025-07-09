fcaf6cde8b4ca2c36f9100251b9f153553de3392
Nathan Gasser <hello@gougoule.ch>
2025-07-09 10:48:45 +0200

    wip

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-var fileName = "main.org"
+var fileName = "main.org";
 
 fileName = process.argv[2] || fileName;
 
@@ -14,153 +14,185 @@ const ReservedKeywords = [
     "function",
     "return",
     "modP",
-    "free"
-]
+    "free",
+];
 
 type TokenType = {
     type: string;
     value: string;
-}
+};
 
-type ValueTypes = { dimentions: number[], type: "int8" | "int16" | "int32" | "int64" | "uint8" | "uint16" | "uint32" | "uint64" | "float" | "double" | "char" | "pointer" }
+type ValueTypes = {
+    dimensions: ExpressionType[];
+    baseType:
+        | "int8"
+        | "int16"
+        | "int32"
+        | "int64"
+        | "uint8"
+        | "uint16"
+        | "uint32"
+        | "uint64"
+        | "float"
+        | "double"
+        | "char"
+        | "pointer";
+};
+var validBaseTypes = [
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "float",
+    "double",
+    "char",
+    "pointer",
+]
 
 type AllocationForPointerType = {
     type: ValueTypes;
-}
+};
 
 type FreeMemoryType = {
     address: ExpressionType;
-}
+};
 
 type ModifyPointerValueType = {
     address: ExpressionType;
     newValue: ExpressionType;
-}
+};
 
 type VariableDeclarationType = {
     type: ValueTypes;
     name: string;
     expression: ExpressionType;
-}
+};
 
 type ModifyVariableType = {
     variable: VariableDeclarationType;
     newExpression: ExpressionType;
-}
+};
 
 type ReallocVariableType = {
     variable: VariableDeclarationType;
     newType: ValueTypes;
-}
+};
 
 type IfType = {
     condition: ExpressionType;
     body: ExpressionType;
-}
+};
 
 type ElseIfType = {
     condition: ExpressionType;
     body: ExpressionType;
-}
+};
 
 type ElseType = {
     body: ExpressionType;
-}
+};
 
 type IfElseChainType = {
     if: IfType;
     elseIfs: ElseIfType[] | null;
     else: ElseType | null;
-}
+};
 
 type WhileType = {
     condition: ExpressionType;
     body: ExpressionType;
-}
+};
 
 type SystemCallType = {
     functionName: string;
     parameters: ExpressionType;
-}
+};
 
 type FunctionParameterType = {
     type: ValueTypes;
     name: string;
-}
+};
 
 type FunctionDeclarationType = {
     name: string;
     returnType: ValueTypes;
     parameters: FunctionParameterType[];
     body: ExpressionType;
-}
+};
 
 type FunctionCallType = {
     function: FunctionDeclarationType;
     parameters: FunctionParameterType[];
-}
+};
 
 type MathExpressionType = {
     left: ExpressionType;
     operator: "+" | "-" | "*" | "/" | "%" | "^";
     right: ExpressionType;
-}
+};
 
-type ReturnType = ExpressionType 
+type ReturnType = ExpressionType;
 
 type VariableType = {
     name: string;
-    location: number[];
-}
+    location: ExpressionType[];
+};
 
-type SingleExpressionType = (
-    {
-        type:
-        "variableDeclaration" |
-        "modifyVariable" |
-        "reallocVariable" |
-        "ifElseChain" |
-        "whileLoop" |
-        "systemCall" |
-        "functionCall" |
-        "functionDeclaration" |
-        "mathExpression" |
-        "return" |
-        "number" |
-        "string" |
-        "variable" |
-        "freeMemory" |
-        "allocationForPointer" |
-        "modifyPointerValue";
-        expression:
-        VariableDeclarationType |
-        ModifyVariableType |
-        ReallocVariableType |
-        IfElseChainType |
-        WhileType |
-        SystemCallType |
-        FunctionCallType |
-        FunctionDeclarationType |
-        MathExpressionType |
-        ReturnType |
-        number |
-        string |
-        VariableType |
-        FreeMemoryType |
-        AllocationForPointerType |
-        ModifyPointerValueType;
-    }
-)
+type SingleExpressionType = {
+    type:
+        | "variableDeclaration"
+        | "modifyVariable"
+        | "reallocVariable"
+        | "ifElseChain"
+        | "whileLoop"
+        | "systemCall"
+        | "functionCall"
+        | "functionDeclaration"
+        | "mathExpression"
+        | "return"
+        | "number"
+        | "string"
+        | "variable"
+        | "freeMemory"
+        | "allocationForPointer"
+        | "modifyPointerValue";
+    expression:
+        | VariableDeclarationType
+        | ModifyVariableType
+        | ReallocVariableType
+        | IfElseChainType
+        | WhileType
+        | SystemCallType
+        | FunctionCallType
+        | FunctionDeclarationType
+        | MathExpressionType
+        | ReturnType
+        | number
+        | string
+        | VariableType
+        | FreeMemoryType
+        | AllocationForPointerType
+        | ModifyPointerValueType;
+};
 
 type ExpressionType = SingleExpressionType[] | SingleExpressionType;
 
-var wordsPossibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+var wordsPossibleChars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
 
-function findStatementInsideDelimiters(str: TokenType[], startDelimiter: string, endDelimiter: string): TokenType[] {
+function findStatementInsideDelimiters(
+    str: TokenType[],
+    startDelimiter: string,
+    endDelimiter: string,
+): TokenType[] {
     let numberOfOpeningDelimiters = 1;
     let numberOfClosingDelimiters = 0;
     let result = [str[0] ?? { type: "unknown", value: "" }];
-    let length = 1
+    let length = 1;
     while (numberOfOpeningDelimiters != numberOfClosingDelimiters) {
         if (str[length] == undefined) return result;
         // @ts-ignore
@@ -178,11 +210,14 @@ function findStatementInsideDelimiters(str: TokenType[], startDelimiter: string,
     return result;
 }
 
-Bun.file(fileName).text().then((content) => {
-    handleFile(content);
-}).catch((error) => {
-    console.error("Error reading file:", error);
-})
+Bun.file(fileName)
+    .text()
+    .then((content) => {
+        handleFile(content);
+    })
+    .catch((error) => {
+        console.error("Error reading file:", error);
+    });
 
 function handleFile(fileContent: string) {
     // start by removing comments
@@ -200,7 +235,7 @@ function handleFile(fileContent: string) {
             if (line.endsWith("*/")) {
                 inCommentBlock = false;
             }
-            lines[i] = null
+            lines[i] = null;
         }
 
         if (!inCommentBlock && line.includes("//")) {
@@ -209,13 +244,17 @@ function handleFile(fileContent: string) {
             lines[i] = line.substring(0, index);
         }
     }
-    const outputLines = lines.filter(line => line != null && line != undefined && line.trim() != "").join("\n");
+    const outputLines = lines
+        .filter(
+            (line) => line != null && line != undefined && line.trim() != "",
+        )
+        .join("\n");
 
     var currentToken = "";
     var insideQuotes = false;
     var tokens: TokenType[] = [{ type: "curlyBrace", value: "{" }];
 
-    var i = 0
+    var i = 0;
     while (i < outputLines.length) {
         const char = outputLines[i];
 
@@ -232,7 +271,7 @@ function handleFile(fileContent: string) {
                     tokens.push({ type: "string", value: currentToken });
                     currentToken = "";
                 }
-                insideQuotes = !insideQuotes
+                insideQuotes = !insideQuotes;
             }
         }
 
@@ -240,72 +279,49 @@ function handleFile(fileContent: string) {
         else if (char == " " || char == "\n" || char == "\t") {
             i++;
             continue;
-        }
-
-        else if (char == "{" || char == "}") {
+        } else if (char == "{" || char == "}") {
             tokens.push({ type: "bracket", value: char });
-        }
-
-        else if (char == "(" || char == ")") {
+        } else if (char == "(" || char == ")") {
             tokens.push({ type: "parenthesis", value: char });
-        }
-
-        else if (char == ",") {
+        } else if (char == ",") {
             tokens.push({ type: "comma", value: char });
-        }
-
-        else if (char == ";") {
+        } else if (char == ";") {
             tokens.push({ type: "semicolon", value: char });
-        }
-
-        else if (char == "[" || char == "]") {
+        } else if (char == "[" || char == "]") {
             tokens.push({ type: "squareBracket", value: char });
-        }
-
-        else if (char == ":") {
+        } else if (char == ":") {
             tokens.push({ type: "colon", value: char });
-        }
-
-        else if (char == "=") {
-            if (outputLines[i + 1] == "=") {
-                tokens.push({ type: "equals", value: "==" });
-                i++; // skip the next character
-            }
-            else {
-                tokens.push({ type: "equal", value: "=" });
-            }
-        }
-
-        else if (char == "+" || char == "-" || char == "*" || char == "/" || char == "%" || char == "^") {
+        } else if (char == "=") {
+            tokens.push({type: "equal", value: char});
+        } else if (
+            char == "+" ||
+            char == "-" ||
+            char == "*" ||
+            char == "/" ||
+            char == "%" ||
+            char == "^"
+        ) {
             tokens.push({ type: "operator", value: char });
-        }
-
-        else if (char == "<" || char == ">") {
+        } else if (char == "<" || char == ">") {
             if (outputLines[i + 1] == "=") {
                 tokens.push({ type: "comparison", value: char + "=" });
                 i++; // skip the next character
             } else {
                 tokens.push({ type: "comparison", value: char });
             }
-        }
-
-        else if (char == "!") {
+        } else if (char == "!") {
             if (outputLines[i + 1] == "=") {
                 tokens.push({ type: "notEqual", value: "!=" });
                 i++; // skip the next character
             } else {
                 tokens.push({ type: "not", value: "!" });
             }
-        }
-
-        else if (char == "&" || char == "|") {
+        } else if (char == "&" || char == "|") {
             if (outputLines[i + 1] == char) {
                 tokens.push({ type: "logicalOperator", value: char + char });
                 i++; // skip the next character
             }
-        }
-
-        else if (wordsPossibleChars.includes(char)) {
+        } else if (wordsPossibleChars.includes(char)) {
             currentToken += char;
 
             //@ts-ignore
@@ -315,38 +331,79 @@ function handleFile(fileContent: string) {
             }
         }
 
-        i++
+        i++;
     }
 
     tokens.push({ type: "curlyBrace", value: "}" });
 
-    /*
-    let fetchedExpression = fetchExpression(tokens);
-    console.log(fetchedExpression);
-    */
 
+    let fetchedExpression: ExpressionType = fetchExpression(tokens.slice(1, -1));
     // output the file to main.out
-    Bun.write("main.out", JSON.stringify(tokens, null, 2))
+    Bun.write("main.out", JSON.stringify(fetchedExpression, null, 2));
 }
 
 function fetchExpression(tokens: TokenType[]): ExpressionType {
     if (tokens[0]?.value != "{") {
-        //TODO: handle the case where a variable could be named after a number
-        // if it doesn't start with one of the reserved keywords, we return a single expression with type return and the expression of type return has a type of whatever the data returned is;
+        return fetchSingleExpression(tokens);
     }
 }
 
-console.log(fetchReturn([{type: "word", value: "123"}]))
-function fetchReturn(tokens: TokenType[]): ReturnType {
-    if (tokens[0]?.type == "string") {
-        // use eval to remove the quotes and the backslashes for formatting purposes
-        return { expression: eval(tokens[0].value), type: "string" };
-    } else if(checkIfNumber(tokens[0]?.value ?? "a")) {
-        return { expression: Number(tokens[0]?.value), type: "number" };
-    } 
-    return fetchExpression(tokens);
+function fetchSingleExpression(tokens: TokenType[]): SingleExpressionType {
+    if (tokens[0]?.type == "word" && tokens[0].value == "var") {
+        return {
+            type: "variableDeclaration",
+            expression: fetchVariableDeclaration(tokens),
+        };
+    } else if (tokens[0]?.type == "word" && checkIfIsNumber(tokens[0].value)) {
+        return {
+            type: "number",
+            expression: Number(tokens[0].value),
+        };
+    }
 }
 
-function checkIfNumber(str: string): boolean {
+function fetchVariableDeclaration(
+    tokens: TokenType[],
+): VariableDeclarationType {
+    if (!validBaseTypes.includes(tokens[1]?.value ?? "")) {
+        throw new Error(
+            `Invalid base type: ${tokens[1]?.value}. Valid types are: ${validBaseTypes.join(", ")}`,
+        );
+    }
+    let tokensToLoop = tokens.slice(2)
+    let dimensions: ExpressionType[] = [];
+    while(tokensToLoop[0]?.value == "[") {
+        let token = findStatementInsideDelimiters(
+            tokensToLoop,
+            "[",
+            "]",
+        );
+        dimensions.push(fetchExpression(token.slice(1, -1)));
+        tokensToLoop = tokensToLoop.slice(token.length);
+    }
+
+    if(checkIfIsNumber(tokensToLoop[0]?.value)) throw new Error("Variable name cannot be a number");
+    if (ReservedKeywords.includes(tokensToLoop[0]?.value as string)) {
+        throw new Error(
+            `Variable name cannot be a reserved keyword: ${tokensToLoop[0]?.value}`,
+        );
+    }
+
+    let variableName = tokensToLoop[0]?.value as string;
+    tokensToLoop = tokensToLoop.slice(1);
+    let expression: ExpressionType = [];
+    expression = fetchExpression(tokensToLoop);
+    return {
+        type: {
+            dimensions: dimensions,
+            baseType: tokens[1]?.value as ValueTypes["baseType"],
+        },
+        name: variableName,
+        expression: expression,
+    }
+}
+
+function checkIfIsNumber(str: string | undefined): boolean {
+    if (str === undefined) return false;
     return !isNaN(Number(str));
 }
